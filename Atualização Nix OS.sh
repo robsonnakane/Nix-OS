61eec28ecf8ab0ae0002dcd9741e25fe031077bd
Robson Nakane <97422340+robsonnakane@users.noreply.github.com>
2023-12-12 13:57:12 -0300

    Update Atualização Nix OS.sh

--- Atualização Nix OS.sh
+++ Atualização Nix OS.sh
@@ -10,14 +10,18 @@
 #sudo nano /home/robsonnakane/Documentos/'Atualização Nix OS.sh'#
 #sudo nano /etc/nixos/configuration.nix
 
+##Alteração do channel para unstable##
+sudo nix-channel --add https://channels.nixos.org/nixos-unstable nixos
+
 ###Atualização do sitema
-sudo nixos-rebuild switch;
+sudo nixos-rebuild switch --update;
+sudo nixos-rebuild switch --upgrade;
 
 ##Instalação dos programas Flatpak##
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; 
-flatpak install flathub com.spotify.Client -y; flatpak install flathub org.videolan.VLC -y; flatpak install flathub com.valvesoftware.Steam -y; flatpak install flathub us.zoom.Zoom -y; flatpak install flathub org.kde.kdenlive -y; flatpak install flathub org.onlyoffice.desktopeditors -y; flatpak install flathub com.skype.Client -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub org.gnome.Extensions -y; flatpak install flathub com.transmissionbt.Transmission -y; flatpak install flathub org.inkscape.Inkscape -y; flatpak install flathub org.mozilla.Thunderbird -y; flatpak install flathub org.raspberrypi.rpi-imager -y; flatpak install flathub org.fedoraproject.MediaWriter -y; flatpak install flathub org.gnome.gedit -y; flatpak install flathub com.google.Chrome -y; flatpak install flathub org.gnome.Firmware -y; flatpak install flathub org.atheme.audacious -y; flatpak install flathub ca.littlesvr.asunder -y;  flatpak install flathub ar.com.tuxguitar.TuxGuitar -y; flatpak install flathub org.gnome.Boxes -y;
+#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; 
+#flatpak install flathub com.spotify.Client -y; flatpak install flathub org.videolan.VLC -y; flatpak install flathub com.valvesoftware.Steam -y; flatpak install flathub us.zoom.Zoom -y; flatpak install flathub org.kde.kdenlive -y; flatpak install flathub org.onlyoffice.desktopeditors -y; flatpak install flathub com.skype.Client -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub org.gnome.Extensions -y; flatpak install flathub com.transmissionbt.Transmission -y; flatpak install flathub org.inkscape.Inkscape -y; flatpak install flathub org.mozilla.Thunderbird -y; flatpak install flathub org.raspberrypi.rpi-imager -y; flatpak install flathub org.fedoraproject.MediaWriter -y; flatpak install flathub org.gnome.gedit -y; flatpak install flathub com.google.Chrome -y; flatpak install flathub org.gnome.Firmware -y; flatpak install flathub org.atheme.audacious -y; flatpak install flathub ca.littlesvr.asunder -y;  flatpak install flathub ar.com.tuxguitar.TuxGuitar -y; flatpak install flathub org.gnome.Boxes -y;
 
 ##Atualização do Flatpak##
-flatpak update -y;
+#flatpak update -y;
 
 systemctl reboot
