0a5f470c65a7f2e2342074ff2297c1c174b5c5c3
Robson Nakane <97422340+robsonnakane@users.noreply.github.com>
2024-01-05 14:20:43 -0300

    Atualização de arquivos

--- Atualização Nix OS.sh
+++ Atualização Nix OS.sh
@@ -17,7 +17,7 @@
 sudo nixos-rebuild switch --upgrade;
 
 ##Instalação dos programas Flatpak##
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; flatpak install flathub com.skype.Client -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub org.gnome.Extensions -y; 
+#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; flatpak install flathub com.skype.Client -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub org.gnome.Extensions -y; 
 
 ##Atualização do Flatpak##
 flatpak update -y;
