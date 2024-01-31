8f78e2be1295708c2bff2c05259b86d4e104b8cc
Robson Nakane <97422340+robsonnakane@users.noreply.github.com>
2024-01-30 22:20:39 -0300

    Add files via upload

--- Atualização Nix OS.sh
+++ Atualização Nix OS.sh
@@ -20,7 +20,7 @@ nix-collect-garbage
 sudo nixos-rebuild switch --upgrade;
 
 ##Instalação dos programas Flatpak##
-#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; flatpak install flathub com.skype.Client -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub org.gnome.Extensions -y; flatpak install flathub com.transmissionbt.Transmission -y;
+#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; flatpak install flathub com.skype.Client -y; flatpak install flathub com.adobe.Flash-Player-Projector -y; flatpak install flathub com.transmissionbt.Transmission -y;
 
 ##Atualização do Flatpak##
 flatpak update -y
