6d9c5d3a63a9e37fa4142e932058fe8817b713ce
Robson Nakane <theblizzard1983@hotmail.com>
2026-08-01 14:09:52 -0300

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,43 +1,53 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  steam
-  wineWowPackages.waylandFull  
+  #steam
+  #wineWowPackages.waylandFull  
   google-chrome
   thunderbird
   firefox
-  gnome.simple-scan
+  simple-scan
   jdk
   btop
   kitty
   spotify
   vlc
   zoom-us
-  libsForQt5.kdenlive
-  onlyoffice-bin
+  kdePackages.kdenlive
+  onlyoffice-desktopeditors
   inkscape
   rpi-imager
   mediawriter
   audacious
   tuxguitar
-  gnome.gnome-boxes
-  libsForQt5.kate
+  gnome-boxes
+  kdePackages.kate
   lsd
   foomatic-db
   bluez
-  gnome.gnome-disk-utility
-  anydesk
-  gnome.nautilus #apenas GNOME without the apps sem a #
-  gnome.gnome-tweaks
+  gnome-disk-utility
+  nautilus
+  gnome-tweaks
   flatpak
-  gnome.gnome-software  
+  gnome-software  
   lshw
   python3
   fastfetch
-  libreoffice-qt
   collision
-  pkgs.linuxKernel.kernels.linux_latest_libre #para computadores mais novos
-  #pkgs.linuxKernel.kernels.linux_zen #para computadores mais antigos ( caso não funcione, não ultilize )
+  distrobox
+  boxbuddy
+  audacity
+  kdePackages.gwenview
+  gimp
+  transmission_4
+  #teams
+  zoom-us
+  telegram-desktop
+  tuxguitar
+  rsync
+  tailscale
+  #pkgs.linuxKernel.kernels.linux_latest_libre #para computadores mais novos
+  pkgs.linuxKernel.kernels.linux_zen #para computadores mais antigos ( caso não funcione, não ultilize )
 
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
@@ -51,8 +61,8 @@
   services.flatpak.enable = true;
 
   #Enable Kernels Packages
-  boot.kernelPackages = pkgs.linuxPackages_latest; #para computadores mais novos
-  #boot.kernelPackages = pkgs.linuxPackages_zen; #para computadores mais antigos
+  #boot.kernelPackages = pkgs.linuxPackages_latest; #para computadores mais novos
+  boot.kernelPackages = pkgs.linuxPackages_zen; #para computadores mais antigos
 
   #Enable an experimental Rust support:
   boot.kernelPatches = [
@@ -65,15 +75,15 @@
    }
   ];
 
-  #Enable Steam
-  programs.steam = {
-  enable = true;
-  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-};
+  ##Enable Steam
+  #programs.steam = {
+  #enable = true;
+  #remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  #dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+#};
 
-  #Enable gamescope for Steam
-  programs.steam.gamescopeSession.enable = true;
+  ##Enable gamescope for Steam
+  #programs.steam.gamescopeSession.enable = true;
   
   #Enable Firewall
   networking.firewall = {
