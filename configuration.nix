44ab34998aa1a9ac775d6d7e8ee1ca2cde921909
Robson Nakane <theblizzard1983@hotmail.com>
2026-08-19 12:24:00 -0300

    Remove Flatpak package installation configuration
    Removed specific Flatpak package installations and management settings.

--- configuration.nix
+++ configuration.nix
@@ -254,18 +254,6 @@ fileSystems."/mnt/sdb1" = {
 ##FlatPaks AutoUpdate
 services.flatpak.enable = true;
 
-# Declara os aplicativos que você quer instalados pelo Flathub
-  services.flatpak.packages = [
-    "com.mattjakeman.ExtensionManager"
-    "com.google.ChromeDev"
-    "com.github.tchx84.Flatseal"
-    "net.mkiol.SpeechNote"
-    "com.github.IsmaelMartinez.teams_for_linux"
-  ];
-
-  # Remove automaticamente Flatpaks instalados manualmente que não estejam listados acima
-  services.flatpak.removeUnmanagedPackages = true;
-
 # Criar o serviço que roda o comando de atualização
 systemd.services.flatpak-auto-update = {
   description = "Atualização Automática dos Flatpaks";
